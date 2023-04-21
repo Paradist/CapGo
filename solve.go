0495ef927fbaf1f31b6bf9ad3e05870698d56d5e
Paradist <84618269+Paradist@users.noreply.github.com>
2023-04-21 16:12:03 +0300

    Update solve.go

--- solve.go
+++ solve.go
@@ -6,12 +6,10 @@ import (
 )
 
 func (c capGoStruct) Solve(task map[string]any) (*CapSolverResponse, error) {
-	log.Println("1")
 	err := c.CheckTaskArguments(task)
 	if err != nil {
 		return nil, err
 	}
-	log.Println("1")
 	resp, err := c.CreateTask(task)
 	if err != nil {
 		return nil, err
@@ -19,9 +17,7 @@ func (c capGoStruct) Solve(task map[string]any) (*CapSolverResponse, error) {
 	if (resp.Status == STATUS_READY) {
 		return resp, nil
 	}
-	log.Println("1")
 	for i := 0; i < TASK_TIMEOUT; i++ {
-		log.Println("32")
 		time.Sleep(time.Second)
 		ok, _ := c.CheckTask(resp.TaskId)
 		if (ok) {
