8d9f5550a003e12094d463247f8610be277b3d6f
Paradist <84618269+Paradist@users.noreply.github.com>
2023-04-22 18:36:42 +0300

    Add files via upload

--- checkTask.go
+++ checkTask.go
@@ -1,16 +1,12 @@
 package capGo
 
-import "log"
-
 func (c capGoStruct) CheckTask(taskId string) (bool, error) {
 	resp, err := c.Request(GET_TASK_PATH, &CapSolverRequest{
 		ClientKey: c.ApiKey,
 		TaskId:    taskId,
 	})
-	log.Println(err)
 	if err != nil {
 		return false, err
 	}
-	log.Println(resp.Status)
 	return resp.Status == STATUS_READY, nil
 }
